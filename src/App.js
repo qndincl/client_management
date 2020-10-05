67bceaa2cf5d6bcff3a6efd41e048ed2efe29b55
qndincl <aaalg1@naver.com>
2020-10-06 01:29:28 +0900

    #1.2.1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React,{ Component } from 'react';
 import Customer from './components/Customer'
 import './App.css';
 
-const customer=[
+const customers=[
 {
   'id':1,
   'image':'http://placeimg.com/64/64/people?t=1601914723874',
@@ -33,31 +33,11 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Customer
-        id={customer[0].id}
-        image={customer[0].image}
-        name = {customer[0].name}
-        birthday={customer[0].birthday}
-        gender={customer[0].gender}
-        job={customer[0].job}
-        />
-        <Customer
-        id={customer[1].id}
-        image={customer[1].image}
-        name = {customer[1].name}
-        birthday={customer[1].birthday}
-        gender={customer[1].gender}
-        job={customer[1].job}
-        />
-        <Customer
-        id={customer[2].id}
-        image={customer[2].image}
-        name = {customer[2].name}
-        birthday={customer[2].birthday}
-        gender={customer[2].gender}
-        job={customer[2].job}
-        />
-        </div>
+      {customers.map(c => {
+      return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+      })}
+      </div>
+      
     );
   }
 }
