d1ba51082a7b88842fa0b01d40d7c2fcd39e6eda
qndincl <aaalg1@naver.com>
2020-10-06 01:41:19 +0900

    #1.2.2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React,{ Component } from 'react';
 import Customer from './components/Customer'
 import './App.css';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root: {
+    width: "100%",
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+});
+
 
 const customers=[
 {
@@ -31,6 +49,34 @@ const customers=[
 
 class App extends Component {
   render() {
+  return (
+  <div>
+  <Table>
+  <TableHead>
+  <TableRow>
+  <TableCell>번호</TableCell>
+  <TableCell>이미지</TableCell>
+  <TableCell>이름</TableCell>
+  <TableCell>생년월일</TableCell>
+  <TableCell>성별</TableCell>
+  <TableCell>직업</TableCell>
+  </TableRow>
+  </TableHead>
+  <TableBody>
+  {customers.map(c => {
+  return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+  })}
+  </TableBody>
+  </Table>
+  </div>
+  );
+  }
+  }
+  
+  
+/*
+class App extends Component {
+  render() {
     return (
       <div>
       {customers.map(c => {
@@ -41,6 +87,7 @@ class App extends Component {
     );
   }
 }
+*/
 
 
 export default App;
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,5 +1,27 @@
 import React from 'react';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
+
+
+class Customer extends React.Component {
+    render() {
+        return (
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src={this.props.image} alt="profile" /></TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.birthday}</TableCell>
+                <TableCell>{this.props.gender}</TableCell>
+                <TableCell>{this.props.job}</TableCell>
+            </TableRow>
+        )
+    }
+}
+
+
+
+/*
 class Customer extends React.Component {
     render() {
         return (
@@ -35,6 +57,6 @@ class CustomerInfo extends React.Component{
         )
     }
 }
-
+*/
   
   export default Customer;
